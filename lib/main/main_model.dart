797610706eeb9e659a1544bd0f200ee4784f3aca
Kyle <147102034+PandaEaterofTrash@users.noreply.github.com>
2024-04-25 11:31:28 -0400

    Update main_model.dart

--- lib/main/main_model.dart
+++ lib/main/main_model.dart
@@ -6,8 +6,11 @@ import 'package:flutter/material.dart';
 // Define MainModel class extending FlutterFlowModel
 class MainModel extends FlutterFlowModel<MainWidget> {
   // State fields for stateful widgets in this page.
+ // unfocusNode: A FocusNode instance used to manage focus within the widget tree.
   final unfocusNode = FocusNode();
+   // isDataUploading: A boolean flag indicating whether data is currently being uploaded.
   bool isDataUploading = false;
+ // uploadedLocalFile: An instance of FFUploadedFile representing the uploaded file. It's initialized with an empty Uint8List.
   FFUploadedFile uploadedLocalFile =
       FFUploadedFile(bytes: Uint8List.fromList([]));
 
